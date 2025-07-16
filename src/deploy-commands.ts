432f9acdf2d2532759b9868038632646afc109ae
Yuki <github@risaton.net>
2025-07-16 23:06:53 +0900

    Implement basic ping command in deploy-commands.ts using SlashCommandBuilder

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -8,5 +8,14 @@ const { Routes } = require('discord-api-types/v9');
 const fs = require('node:fs');
 
 
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+let commands = [];
+// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+const commandPing = new SlashCommandBuilder()
+.setName('ping')
+.setDescription('Replies with Pong!');
+
+commands.push(commandPing)
+
+const rest = new REST({ version: '9' }).setToken(TOKEN);
+
